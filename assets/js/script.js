4390111b423535c83f0b7da8fef66d5c009dbc84
abhinand9772 <100460106+abhinand9772@users.noreply.github.com>
2023-01-27 15:51:12 +0530

    updt

--- assets/js/script.js
+++ assets/js/script.js
@@ -828,8 +828,7 @@
     window.onload = function(){
         swal.fire({
           title: "TestWorx",
-          text: "Software testing company",
-          footer:
+          html:
             'Have any quires? <a href="contact.html"> Get in touch with us</a>',
         });
       setTimeout(5000);
