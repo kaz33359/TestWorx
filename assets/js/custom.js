cc43f29c93d411239003835812801e00e04aa5ca
abhinand9772 <100460106+abhinand9772@users.noreply.github.com>
2023-01-28 13:57:30 +0530

    update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -22,20 +22,3 @@ form.addEventListener('submit', function(event){
    contact();
 })
 
-
-setCookie = (cName, cValue, expdays) => {
-  let date = new Date();
-  date.setTime(date.getTime() + (expDays * 24 * 60 * 60 * 1000));
-  const expires = "expires=" + date.toUTCString();
-  document.cookie = cName + "=" + cValue + "; " + expires ;
-}
-
-getCookie = (cName) => {
-  const name = cName + "=";
-  
-}
-
-document.querySelector("#cookies-btn").addEventListener("click", () =>{
-  document.querySelector("#cookies").style.display = "none";
-  setCookie("cookie", true, 30);
-})
