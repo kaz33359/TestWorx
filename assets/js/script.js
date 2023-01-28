68d0a1313b9c737017888135bf61e5b3ba789b52
abhinand9772 <100460106+abhinand9772@users.noreply.github.com>
2023-01-28 10:14:13 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -827,9 +827,13 @@
 
     window.onload = function(){
         swal.fire({
-          title: "TestWorx",
-          html:
-            'Have any quires? <a href="contact.html"> Get in touch with us</a>',
+          title: "Software Testing Company",
+            html:
+              'Have any quires? <a href="contact.html"> Get in touch with us</a>',
+          imageUrl:
+            "https://kaz33359.github.io/TestWorx/assets/images/testworx/logo/dark_logo.png",
+          imageWidth: 300,
+          imageHeight: 40,
         });
       setTimeout(3000);
     }
