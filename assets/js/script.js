3179eb4e4d39397f629b13b6d1210d178f891623
abhinand9772 <100460106+abhinand9772@users.noreply.github.com>
2023-01-30 12:33:28 +0530

    updt

--- assets/js/script.js
+++ assets/js/script.js
@@ -828,7 +828,7 @@
     setTimeout(function(){
         swal.fire({
           title: "Software Testing Company",
-          html: 'Have any queries? <a href="contact.html"> Get in touch with us</a>',
+          html: 'Have any queries? <a href="../contact.html"> Get in touch with us</a>',
           imageUrl:
             "https://kaz33359.github.io/TestWorx/assets/images/testworx/logo/dark_logo.png",
           imageWidth: 300,
