40d2dee4201c134c3d7c843582340c2e9315923e
abhinand9772 <100460106+abhinand9772@users.noreply.github.com>
2023-02-01 17:37:54 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -825,16 +825,16 @@
 
 })(window.jQuery);
 
-    setTimeout(function(){
-        swal.fire({
-          title: "Software Testing Company",
-          html: 'Have any queries? <a href="../contact.html"> Get in touch with us</a>',
-          imageUrl:
-            "https://kaz33359.github.io/TestWorx/assets/images/testworx/logo/dark_logo.png",
-          imageWidth: 350,
-          imageHeight: 40,
-        });
-    },15000);
+    // setTimeout(function(){
+    //     swal.fire({
+    //       title: "Software Testing Company",
+    //       html: 'Have any queries? <a href="../contact.html"> Get in touch with us</a>',
+    //       imageUrl:
+    //         "https://kaz33359.github.io/TestWorx/assets/images/testworx/logo/dark_logo.png",
+    //       imageWidth: 350,
+    //       imageHeight: 40,
+    //     });
+    // },15000);
 
     // window.onload = function(){
     //     swal.fire({
